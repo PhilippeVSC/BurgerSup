c51fd6e816319de224720764a0b5ba78723b311e
PhilippeVSC <philippe.vivesc@gmail.com>
2023-04-21 13:57:52 +0200

    main in progress

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function switchSelected() {
     const calendarSelect = document.querySelector(".calendar-select");
     const usersSelect = document.querySelector(".users-select");
+
+    const usersPage = document.querySelector(".users-page");
   
     if (!calendarSelect.classList.contains("selected")) {
       calendarSelect.classList.add("selected");
       usersSelect.classList.remove("selected");
+      usersPage.style.transform = 'translateY(-100%)';
     }
     else if (!usersSelect.classList.contains("selected")) {
       usersSelect.classList.add("selected");
       calendarSelect.classList.remove("selected");
+      usersPage.style.transform = 'translateY(0)';
     }
 }
