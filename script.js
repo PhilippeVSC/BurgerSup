3d2c09c73e59b9310eb68445f8eab563531e24d2
PhilippeVSC <philippe.vivesc@gmail.com>
2023-04-21 16:04:54 +0200

    Main left part almost finished & right part started

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ function switchSelected() {
     const calendarSelect = document.querySelector(".calendar-select");
     const usersSelect = document.querySelector(".users-select");
 
+    const calendarPage = document.querySelector(".calendar-page");
     const usersPage = document.querySelector(".users-page");
   
     if (!calendarSelect.classList.contains("selected")) {
       calendarSelect.classList.add("selected");
       usersSelect.classList.remove("selected");
       usersPage.style.transform = 'translateY(-100%)';
+      calendarPage.style.transform = 'translateY(0)';
     }
     else if (!usersSelect.classList.contains("selected")) {
       usersSelect.classList.add("selected");
       calendarSelect.classList.remove("selected");
       usersPage.style.transform = 'translateY(0)';
+      calendarPage.style.transform = 'translateY(100%)';
     }
 }
