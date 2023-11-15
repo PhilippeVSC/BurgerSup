0d3f5e7a77598ae8b774a6791bfaaf7a80884bd4
Ilias TRAGHA <104205016+BDF172@users.noreply.github.com>
2023-11-15 15:49:22 +0100

    Update delete_user.php
    Fixed an sql injection vulnerbility

--- delete_user.php
+++ delete_user.php
@@ -4,8 +4,9 @@
     $id = $_GET['id'];
 
     // Supprimer l'utilisateur de la table bs_user
-    $sql = "DELETE FROM bs_user WHERE user_id = $id";
-    $bdd->query($sql);
+    $stmt = $bdd->prepare("DELETE FROM bs_user WHERE user_id = ?");
+    $stmt->bind_param("s", $id);
+    $stmt->execute();
 
     // Supprimer toutes les données de l'utilisateur supprimé de la table bs_data
     $sql = "DELETE FROM bs_data WHERE user_id = $id";
